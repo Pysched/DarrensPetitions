92ce12f78239ffbf03791fd8212c6cd4ca130f74
Pysched <48335713+Pysched@users.noreply.github.com>
2023-11-16 19:34:19 +0000

    Initial commit - Project Setup

---
+++ src/main/java/com/example/DarrensPetitions/DarrensPetitionsApplication.java
@@ -0,0 +1,17 @@
+package com.example.DarrensPetitions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Collections;
+
+@SpringBootApplication
+public class DarrensPetitionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication app = new SpringApplication(DarrensPetitionsApplication.class);
+		app.setDefaultProperties(Collections.singletonMap("server.port", "8087"));
+		app.run(args);
+	}
+
+}
