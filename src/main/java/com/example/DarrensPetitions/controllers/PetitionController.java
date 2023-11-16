fd324c43046fc6e4e5f6d022050f4ff07c20109a
Pysched <48335713+Pysched@users.noreply.github.com>
2023-11-16 21:44:52 +0000

    Created create.html base html page using the fragments elements Updated index to include the navigation from fragments Updated the PetitionController to include model for create

--- src/main/java/com/example/DarrensPetitions/controllers/PetitionController.java
+++ src/main/java/com/example/DarrensPetitions/controllers/PetitionController.java
@@ -6,10 +6,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class PetitionController {
+
     @RequestMapping("/")
+    // Model is used to pass data to the view
     public String index(Model model){
+        // Add attributes to the model
         model.addAttribute("title", "Darren's Petitions");
         model.addAttribute("pageTitle", "Index Page");
-        return "index";
+        return "index"; // Returns the view name index to render the HTML page
+    }
+
+    @RequestMapping("/create")
+    public String create(Model model){
+        model.addAttribute("title", "Create a Petition");
+        model.addAttribute("pageTitle", "Create Page");
+        return "create"; // Returns the view name create to render the HTML page
     }
 }
