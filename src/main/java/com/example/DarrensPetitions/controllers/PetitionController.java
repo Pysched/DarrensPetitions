db3cc26b50c1bdac85596437b915a77efaacd031
Pysched <48335713+Pysched@users.noreply.github.com>
2023-11-17 00:27:04 +0000

    Create.html - added in description and author as inputs required from user
    fragments.html - updated id's for future active state change
    index.html - added in unless there is results - display the following message, added clickable links for results
    Created the Petition class - title, description, author, and signatures - setters, getters, constructors and a toString
    Updated the create post method to capture the values from the form elements, create a new petition object and populate it with the form data, add new petition to list of petitions
    Added result mapping page, the mapping is passing in the title of the result
    loop through the list to match the correct one
    if found add to the model and return the result page.
    Added result page - Populate a card with the title, description and author  and total amount of signatures - this part is a future consideration.

--- src/main/java/com/example/DarrensPetitions/controllers/PetitionController.java
+++ src/main/java/com/example/DarrensPetitions/controllers/PetitionController.java
@@ -2,6 +2,7 @@ package com.example.DarrensPetitions.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,7 +12,7 @@ import java.util.List;
 
 @Controller
 public class PetitionController {
-    static List<String> petitions = new ArrayList<>();
+    static List<Petition> petitions = new ArrayList<>(); // List to store petition objects
     @RequestMapping("/")
     public String index(Model model){
         model.addAttribute("title", "Darren's Petitions");
@@ -28,9 +29,17 @@ public class PetitionController {
     }
 
     @RequestMapping(value="create", method = RequestMethod.POST)
-    public String processCreate(@RequestParam String petitionTitle){
-        petitions.add(petitionTitle);
-        return "redirect:";
+    public String processCreate(
+            @RequestParam String petitionTitle,
+            @RequestParam String petitionDescription,
+            @RequestParam String petitionAuthor){
+        Petition Petition = new Petition(); //
+        Petition.setPetitionTitle(petitionTitle);
+        Petition.setPetitionDescription(petitionDescription);
+        Petition.setPetitionAuthor(petitionAuthor);
+
+        petitions.add(Petition); // Add the petition to the list of petitions
+        return "redirect:"; // Redirect to index page
     }
 
     @RequestMapping("/search")
@@ -39,10 +48,26 @@ public class PetitionController {
         model.addAttribute("pageTitle", "Search Page");
         return "search";
     }
-    @RequestMapping("/result")
-    public String result(Model model){
+    @RequestMapping("/result/{petitionTitle}")
+    public String result(@PathVariable String petitionTitle, Model model){
         model.addAttribute("title", "Review Petition Result");
         model.addAttribute("pageTitle", "Result Page");
-        return "result";
+
+        Petition isPetition = null;
+        for (Petition petition : petitions ) { // loop through the list of petitions to find the petition with the matching title
+            if (petition.getPetitionTitle().equals(petitionTitle)) {
+                isPetition = petition;
+                break;
+            }
+        }
+
+        if (isPetition != null) { // if the petition is found, add it to the model
+            model.addAttribute("petition", isPetition);
+            return "result";
+        } else {
+            // Handle invalid petitionTitle - return to index page
+            return "index";
+        }
+
     }
 }
