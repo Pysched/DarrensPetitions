84b955b8a3f0bb53421cf0b18035a1a5c71cbd20
Pysched <48335713+Pysched@users.noreply.github.com>
2023-11-19 01:33:30 +0000

    Updated PetitionController - updated RequestMapping to GetMapping where appropriate - added comments Added boostrap classes to result Updated search page method to POST - error fix

--- src/main/java/com/example/DarrensPetitions/controllers/PetitionController.java
+++ src/main/java/com/example/DarrensPetitions/controllers/PetitionController.java
@@ -5,14 +5,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Controller
 public class PetitionController {
     static List<Petition> petitions = new ArrayList<>(); // List to store petition objects
-    @RequestMapping("/")
+    @GetMapping("/")
     public String index(Model model){
         model.addAttribute("title", "Darren's Petitions");
         model.addAttribute("pageTitle", "View Petitions");
@@ -26,36 +25,35 @@ public class PetitionController {
         return "index";
     }
 
-    @RequestMapping(value="create", method = RequestMethod.GET)
+    @GetMapping("create")
     public String create(Model model){
         model.addAttribute("title", "Create a Petition");
         model.addAttribute("pageTitle", "Create Petition");
         return "create";
     }
 
-    @RequestMapping(value="create", method = RequestMethod.POST)
+    @PostMapping("/create")
     public String processCreate(
             @RequestParam String petitionTitle,
             @RequestParam String petitionDescription,
             @RequestParam String petitionAuthor){
-        Petition Petition = new Petition(petitionTitle, petitionDescription, petitionAuthor); // Create a new petition object
-        petitions.add(Petition); // Add the petition to the list of petitions
+        Petition petition = new Petition(petitionTitle, petitionDescription, petitionAuthor);
+        petitions.add(petition); // Add the petition to the list of petitions
         return "redirect:/"; // Redirect to index page
     }
 
-    @GetMapping("/result/{petitionTitle}")
+    @GetMapping("/result/{petitionTitle}")// handle the GET request to display the result of the search
     public String result(@PathVariable String petitionTitle, Model model){
         model.addAttribute("title", "Review Petition Result");
         model.addAttribute("pageTitle", "Result Page");
 
         Petition isPetition = null;
-        for (Petition petition : petitions ) { // loop through the list of petitions to find the petition with the matching title
+        for (Petition petition : petitions) { // loop through the list of petitions to find the petition with the matching title
             if (petition.getPetitionTitle().equals(petitionTitle)) {
                 isPetition = petition;
                 break;
             }
         }
-
         if (isPetition != null) { // if the petition is found, add it to the model
             model.addAttribute("petition", isPetition);
             return "result";
@@ -81,14 +79,14 @@ public class PetitionController {
     }
 
     @GetMapping("/search") // Handle the initial search page request
-    public String searchPage(Model model){
+    public String searchPage(Model model) {
         model.addAttribute("title", "Search for a Petition");
         model.addAttribute("pageTitle", "Search Petitions");
         return "search"; // Map to "search.html"
     }
 
-    @PostMapping("/search") // Handle the POST request to search for a petition
-    public String searchRedirect(@RequestParam String searchTerm){
+    @PostMapping("/search") // Handle the POST request for search and redirect to results page
+    public String searchRedirect(@RequestParam String searchTerm) {
         return "redirect:/results?searchTerm=" + searchTerm;
     }
 
